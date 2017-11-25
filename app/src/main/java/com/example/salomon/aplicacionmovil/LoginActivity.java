fa0f12054c34fd6f8e535d5545d7b811aeca4560
Luis Ricardo <luis-ls17@hotmail.com>
2017-11-25 02:37:47 -0500

    menu con material design

--- app/src/main/java/com/example/salomon/aplicacionmovil/LoginActivity.java
+++ app/src/main/java/com/example/salomon/aplicacionmovil/LoginActivity.java
@@ -3,11 +3,13 @@ package com.example.salomon.aplicacionmovil;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "MD";
     Button btnIniciar;
     TextView txtRegistrar;
     @Override
@@ -17,11 +19,22 @@ public class LoginActivity extends AppCompatActivity {
         btnIniciar = findViewById(R.id.login_btn_inicio);
         txtRegistrar = findViewById(R.id.login_lbl_registrar);
 
-        btnIniciar.setOnClickListener(new View.OnClickListener() {
+        /*btnIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO Auto-generated method stub
                 Intent i = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivity(i);
+            }
+        });*/
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO Auto-generated method stub
+                Log.i(TAG, "onClick:    INICAR :P");
+                Intent i = new Intent(getApplicationContext(),MenuDesignActivity.class);
+                Log.i(TAG, "onClick:    INICAR 222 :P");
                 startActivity(i);
             }
         });
---
+++ app/src/main/java/com/example/salomon/aplicacionmovil/MenuDesignActivity.java
@@ -0,0 +1,121 @@
+package com.example.salomon.aplicacionmovil;
+
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MenuDesignActivity extends AppCompatActivity {
+    /**
+     * Instancia del drawer
+     */
+    private DrawerLayout drawerLayout;
+
+    /**
+     * Titulo inicial del drawer
+     */
+    private String drawerTitle;
+    private static final String TAG = "MD";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate:    INICAR 333 :P");
+        super.onCreate(savedInstanceState);
+        // Log.i(TAG, "onCreate:    INICAR 444 :P");
+
+        Log.i(TAG, "onCreate:    INICAR 555 :P  ");
+        setContentView(R.layout.activity_menu_design);
+
+        Log.i(TAG, "onCreate: :P");
+
+        setToolbar();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            // Añadir carácteristicas
+            setupDrawerContent(navigationView);
+        }
+        drawerTitle = getResources().getString(R.string.home_item);
+        if (savedInstanceState == null) {
+            // Seleccionar item
+            selectItem(drawerTitle);
+        }
+    }
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMD);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner ícono del drawer toggle
+            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    // Marcar item presionado
+                    menuItem.setChecked(true);
+                    // Crear nuevo fragmento
+                    String title = menuItem.getTitle().toString();
+                    selectItem(title);
+                    return true;
+                }
+            }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            getMenuInflater().inflate(R.menu.md_nav_menu, menu);
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void selectItem(String title) {
+        // Enviar título como arguemento del fragmento
+        Bundle args = new Bundle();
+        args.putString(PlaceHolderFragment.ARG_SECTION_TITLE, title);
+
+        Fragment fragment = PlaceHolderFragment.newInstance(title);
+        fragment.setArguments(args);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager
+                .beginTransaction()
+                .replace(R.id.main_content, fragment)
+                .commit();
+
+        drawerLayout.closeDrawers(); // Cerrar drawer
+
+        setTitle(title); // Setear título actual
+
+    }
+
+}
