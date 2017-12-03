2d5e690f01f5c84b26ca91b9a4707246953ac564
Salomon Memenza <salomemenza@gmail.com>
2017-12-03 06:36:16 -0500

    Validacion en Login y Correccion de error
    Validacion en activity Login y Carreccion de error cuando un usuario no registrado ingresa.

--- app/src/main/java/com/example/salomon/aplicacionmovil/LoginActivity.java
+++ app/src/main/java/com/example/salomon/aplicacionmovil/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.salomon.aplicacionmovil;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,24 +19,23 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "MD";
     Button btnIniciar;
     TextView txtRegistrar;
-
     EditText txtUsuario, txtPassword;
+
+    private TextInputLayout lytUsuario, lytPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnIniciar = findViewById(R.id.login_btn_inicio);
-        txtRegistrar = findViewById(R.id.login_lbl_registrar);
-
-        txtUsuario = findViewById(R.id.login_txt_usuario);
-        txtPassword = findViewById(R.id.login_txt_password);
-
-
+        constructObject();
 
         btnIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!validateForm()) {
+                    return;
+                }
+
                 UsuarioDAO usuarioDAO=new UsuarioDAO(view.getContext());
                 Usuario objUsuario = usuarioDAO.obtenerByUser(txtUsuario.getText().toString());
 
@@ -42,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
                 Log.i(TAG, "txt Pasword: "+txtPassword.getText().toString());
 
                 String pass = txtPassword.getText().toString();
-                if (! pass.equals(objUsuario.getPassword().toString())){
+                if (TextUtils.isEmpty(objUsuario.getPassword()) || !pass.equals(objUsuario.getPassword().toString())){
                     Toast.makeText(LoginActivity.this, "Contraseña Incorrecta o usuario no registrado", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -62,5 +63,36 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(iRegistrar);
             }
         });
+    }
+
+    private boolean validateForm() {
+        if (txtUsuario.getText().toString().trim().isEmpty()) {
+            lytUsuario.setError("Ingrese un Usuario");
+            txtUsuario.requestFocus();
+            return false;
+        } else {
+            lytUsuario.setErrorEnabled(false);
+        }
+
+        if (txtPassword.getText().toString().trim().isEmpty()) {
+            lytPassword.setError("Ingrese una contraseña");
+            txtPassword.requestFocus();
+            return false;
+        } else {
+            lytPassword.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private void constructObject(){
+        btnIniciar = findViewById(R.id.login_btn_inicio);
+        txtRegistrar = findViewById(R.id.login_lbl_registrar);
+
+        txtUsuario = findViewById(R.id.login_txt_usuario);
+        txtPassword = findViewById(R.id.login_txt_password);
+
+        lytUsuario = findViewById(R.id.login_lyt_usuario);
+        lytPassword = findViewById(R.id.login_lyt_password);
     }
 }
